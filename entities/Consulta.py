250ae1f259464b41227b5fe8cbef09271b414775
crispimUchoa <francisco,crispim@aluno.uece.br>
2025-02-09 18:13:15 -0300

    cria classe Descricao e atualiza atributo descricoes na classe Consulta

--- entities/Consulta.py
+++ entities/Consulta.py
@@ -2,10 +2,11 @@ from Medico import Medico
 from Usuario import Usuario
 
 class Consulta:
-    def __init__(self, id, paciente, medico, data, preco, nota=0):
+    def __init__(self, id, paciente, medico, data, preco, descricoes, nota=0):
         self.id = id
         self.paciente = paciente
         self.medico = medico
         self.data = data
         self.preco = preco
+        self.descricoes = descricoes
         self.nota = nota
---
+++ entities/Descricao.py
@@ -0,0 +1,4 @@
+class Descricao:
+    def __init__(self, consulta, descricao):
+        self.consulta = consulta
+        self.descricao = descricao
