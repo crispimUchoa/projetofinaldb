f2e36e9f954e7414e9c121f8ce33d394acfc1dc7
crispimUchoa <francisco,crispim@aluno.uece.br>
2025-02-09 18:05:17 -0300

    cria classe Horario, adiciona atributo horarios a classe Medico

---
+++ entities/Horario.py
@@ -0,0 +1,4 @@
+class Horario:
+    def __init__(self, id_medico, horario):
+        self.id_medico = id_medico
+        self.horario = horario
--- entities/Medico.py
+++ entities/Medico.py
@@ -1,8 +1,10 @@
 from Usuario import Usuario
 
+
 class Medico(Usuario):
-    def __init__(self, id, nome, senha, email, especializacao, avaliacao=0, atende_plantao=True):
+    def __init__(self, id, nome, senha, email, especializacao, horarios, avaliacao=0, atende_plantao=True):
         super().__init__(id, nome, senha, email)
         self.especializacao = especializacao
         self.avaliacao=avaliacao
         self.atende_plantao=atende_plantao
+        self.horarios = horarios
