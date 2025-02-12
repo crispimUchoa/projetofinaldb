43ee7bca7b658827021f8f2410595169f60a9a69
Henrique Chuvas <henriquechuvas@hotmail.com>
2025-02-12 16:51:42 -0300

    Adicionada tela de login, mudado o style.css, adicionadas rotas, adcionados html do cadastro e home do paciente

--- server.py
+++ server.py
@@ -1,8 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template('base/index.html')
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        email = request.form['email-input']
+        password = request.form['password-input']
+        if email == "1@1" and password == "2":
+            return paciente_home()
+    return render_template("login.html")
 
+@app.route("/cadastro")
+def cadastro():
+    return render_template("cadastro.html")
+
+@app.route("/paciente_home")
+def paciente_home():
+    return render_template("paciente_home.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
