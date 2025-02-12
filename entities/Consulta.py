a3185a5988cd98afbc285c2b41f18f47ab4dc1c2
crispimUchoa <francisco,crispim@aluno.uece.br>
2025-02-12 19:11:12 -0300

    corrige atributos das classes

--- entities/Consulta.py
+++ entities/Consulta.py
@@ -1,9 +1,9 @@
 
 class Consulta:
-    def __init__(self, id, id_paciente, id_medico, data, preco, descricoes, nota=0):
+    def __init__(self, id, paciente, medico, data, preco, descricoes, nota=0):
         self.id = id
-        self.id_paciente = id_paciente
-        self.id_medico = id_medico
+        self.paciente = paciente
+        self.medico = medico
         self.data = data
         self.preco = preco
         self.descricoes = descricoes
