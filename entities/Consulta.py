04b7d029f9f9a2c9d8e11fd39c148110bc8e0fde
crispimUchoa <francisco,crispim@aluno.uece.br>
2025-02-17 15:12:57 -0300

    adiciona atributo prescrições a classe consulta

--- entities/Consulta.py
+++ entities/Consulta.py
@@ -1,6 +1,6 @@
 
 class Consulta:
-    def __init__(self, id, paciente, medico, data, preco, descricoes, nota=0):
+    def __init__(self, id, paciente, medico, data, preco, descricoes, nota=0, prescricoes = ''):
         self.id = id
         self.paciente = paciente
         self.medico = medico
@@ -8,3 +8,4 @@ class Consulta:
         self.preco = preco
         self.descricoes = descricoes
         self.nota = nota
+        self.prescricoes = prescricoes
